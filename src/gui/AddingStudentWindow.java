81066c22f0233fa5ca9db83801b31106fafea3f6
Yılmaz Sönmez <ysonmez824@gmail.com>
2025-12-18 17:51:56 +0300

    I don't know. Is it even worth to spent time on?
    People already made thousands of student management apps. And I am no different no more. I need to make a better project. also added add student button.

--- src/gui/AddingStudentWindow.java
+++ src/gui/AddingStudentWindow.java
@@ -1,22 +1,34 @@
 package gui;
 
-import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import services.PutService;
 
 public class AddingStudentWindow extends JDialog{
 	
 	PutService putService;
 	
+	JTextField name = new JTextField() 
+			, age = new JTextField();
+	
 	protected AddingStudentWindow(MainWindow main ,PutService put){
 		this.putService = put;
 		
-		setSize(100, 100);
+		setSize(200, 200);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		setLayout(new GridBagLayout());
+		setLayout(new GridLayout(3, 3));
 		
 		setLocationRelativeTo(main);
 		setModal(true);
@@ -26,10 +38,33 @@ public class AddingStudentWindow extends JDialog{
 	
 	public void addComponents() {
 		
-		Object c = new GridBagConstraints();
+		add(new JLabel("Name-Surname"));
+		add(name);
+		add(new JPanel());
 		
+		add(new JLabel("Age"), 2);
+		add(age);
+		add(new JPanel());
 		
+		add(new JPanel());
 		
+		JButton but = new JButton("Save");
+		
+		but.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					putService.putStudent(name.getText(), Integer.parseInt(age.getText()));
+					dispose();
+				}catch(NumberFormatException n) {
+					n = new NumberFormatException("Age must be a number!");
+					new Error(n.getMessage(), n.getCause());
+				}
+			}
+		});
+		
+		add(but);
 	}
 
 }
